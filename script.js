f3cfc8ff40f9412a51107846a394e8b363e08953
Alex Buly <asbuly@icloud.com>
2024-01-03 21:53:12 -0500

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [] // Book items 
+
+function Book() {
+    // author, title, pages, read
+}
+
+function addBooktoLibrary() {
+    /* A button is used to add a new book to the library. 
+    Form input includes author, title pages and read */
+}
+
+// loop through array 
+// delete book 
