1929d71873429325f250b73278b042f74a841968
Alex Buly <asbuly@icloud.com>
2024-01-04 21:33:37 -0500

    Add mock card display

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+const card1 = document.querySelector(".card1");
+const card2 = document.querySelector(".card2");
+const card3 = document.querySelector(".card3");
+const card4 = document.querySelector(".card4")
+
 const myLibrary = [] // Book items 
 
-function Book() {
+function Book(title, author, pages, read) {
     // author, title, pages, read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => {
+        return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+    }
 }
+
+const theHobbit = new Book("The Hobbitt","J.J.R. Tolkien", "295", "not read yet");
+card1.textContent = theHobbit.info();
+
+const harryPotter = new Book("Harry Potter and the Goblet of Fire ", "J.K. Rowling", "636", "read");
+card2.textContent = harryPotter.info();
+
+const hungerGames = new Book("The Hunger Games", "Suzanne Collins", "374", "read");
+card3.textContent = hungerGames.info();
+
+const forestGump = new Book("Forest Gump", "Winston Groom", "228", "read");
+card4.textContent = forestGump.info();
 
 function addBooktoLibrary() {
     /* A button is used to add a new book to the library. 
-    Form input includes author, title pages and read */
+    Form input includes author, title, pages and read */
 }
 
 // loop through array
