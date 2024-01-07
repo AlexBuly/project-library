636617eb89fb00584b4fa3b45c07d3631ed18689
Alex Buly <asbuly@icloud.com>
2024-01-06 20:41:20 -0500

    Add form used to add new books

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+const container = document.querySelector(".container")
 const card1 = document.querySelector(".card1");
 const card2 = document.querySelector(".card2");
 const card3 = document.querySelector(".card3");
 const card4 = document.querySelector(".card4");
 
-const myLibrary = ["The Hobbitt","J.J.R. Tolkien", "295", "not read yet"]; // Book items 
+const myLibrary = []; // Book items 
 
 function Book(title, author, pages, read) {
     // author, title, pages, read
@@ -28,10 +29,65 @@ card3.textContent = hungerGames.info();
 const forestGump = new Book("Forest Gump", "Winston Groom", "228", "read");
 card4.textContent = forestGump.info();
 
-function addBooktoLibrary() {
-    /* A button is used to add a new book to the library. 
-    Form input includes author, title, pages and read */
-}
-// loop through array
-// contents of form or array returned from the constructor
-// delete book 
+const form = document.createElement("form");
+const fieldset = document.createElement("fieldset");
+form.setAttribute("method", "post");
+form.setAttribute("action", "/");
+form.appendChild(fieldset);
+container.appendChild(form);
+
+const br = document.createElement("br");
+
+const title = document.createElement("input");
+title.type = "text"
+title.id = "title";
+title.name= "title";
+const tLabel = document.createElement("label");
+tLabel.htmlFor = "title";
+tLabel.textContent = "Title: ";
+fieldset.appendChild(tLabel)
+fieldset.appendChild(title);
+fieldset.appendChild(br);
+
+const author = document.createElement("input");
+author.type = "text";
+author.id = "author";
+author.name = "author";
+const aLabel = document.createElement("label");
+aLabel.htmlFor = "author";
+aLabel.textContent = "Author: ";
+fieldset.appendChild(aLabel);
+fieldset.appendChild(author);
+fieldset.appendChild(br.cloneNode());
+
+const pages = document.createElement("input");
+pages.type = "number";
+pages.id = "pages";
+pages.name = "pages";
+const pLabel = document.createElement("label");
+pLabel.htmlFor = "pages";
+pLabel.textContent = "Pages: ";
+fieldset.appendChild(pLabel);
+fieldset.appendChild(pages);
+fieldset.appendChild(br.cloneNode());
+
+const read = document.createElement("input");
+read.type = "text";
+read.id = "read";
+read.name = "read";
+const rLabel = document.createElement("label");
+rLabel.htmlFor = "read";
+rLabel.textContent = "Read: ";
+fieldset.appendChild(rLabel);
+fieldset.appendChild(read);
+fieldset.appendChild(br.cloneNode());
+
+const post = document.createElement("button");
+post.type = "sumbit";
+post.textContent = "Add book";
+fieldset.appendChild(post); 
+
+
+
+
+
