8b2185edfb0ae3b8e4a71538cdc09e4d1c76c936
Alex Buly <asbuly@icloud.com>
2024-01-06 23:01:33 -0500

    Add button to display form and show data on card

--- script.js
+++ script.js
@@ -29,68 +29,71 @@ card3.textContent = hungerGames.info();
 const forestGump = new Book("Forest Gump", "Winston Groom", "228", "read");
 card4.textContent = forestGump.info();
 
-const form = document.createElement("form");
-const fieldset = document.createElement("fieldset");
-form.setAttribute("method", "post");
-form.setAttribute("action", "/");
-form.appendChild(fieldset);
-container.appendChild(form);
+const addBooktoLibrary = () => {
+    const form = document.createElement("form");
+    const fieldset = document.createElement("fieldset");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "/");
+    form.appendChild(fieldset);
+    container.appendChild(form);
 
-const br = document.createElement("br");
+    const br = document.createElement("br");
 
-const title = document.createElement("input");
-title.type = "text"
-title.id = "title";
-title.name= "title";
-const tLabel = document.createElement("label");
-tLabel.htmlFor = "title";
-tLabel.textContent = "Title: ";
-fieldset.appendChild(tLabel)
-fieldset.appendChild(title);
-fieldset.appendChild(br);
+    const title = document.createElement("input");
+    title.type = "text"
+    title.id = "title";
+    title.name= "title";
+    const tLabel = document.createElement("label");
+    tLabel.htmlFor = "title";
+    tLabel.textContent = "Title: ";
+    fieldset.appendChild(tLabel)
+    fieldset.appendChild(title);
+    fieldset.appendChild(br);
 
-const author = document.createElement("input");
-author.type = "text";
-author.id = "author";
-author.name = "author";
-const aLabel = document.createElement("label");
-aLabel.htmlFor = "author";
-aLabel.textContent = "Author: ";
-fieldset.appendChild(aLabel);
-fieldset.appendChild(author);
-fieldset.appendChild(br.cloneNode());
+    const author = document.createElement("input");
+    author.type = "text";
+    author.id = "author";
+    author.name = "author";
+    const aLabel = document.createElement("label");
+    aLabel.htmlFor = "author";
+    aLabel.textContent = "Author: ";
+    fieldset.appendChild(aLabel);
+    fieldset.appendChild(author);
+    fieldset.appendChild(br.cloneNode());
 
-const pages = document.createElement("input");
-pages.type = "number";
-pages.id = "pages";
-pages.name = "pages";
-const pLabel = document.createElement("label");
-pLabel.htmlFor = "pages";
-pLabel.textContent = "Pages: ";
-fieldset.appendChild(pLabel);
-fieldset.appendChild(pages);
-fieldset.appendChild(br.cloneNode());
+    const pages = document.createElement("input");
+    pages.type = "number";
+    pages.id = "pages";
+    pages.name = "pages";
+    const pLabel = document.createElement("label");
+    pLabel.htmlFor = "pages";
+    pLabel.textContent = "Pages: ";
+    fieldset.appendChild(pLabel);
+    fieldset.appendChild(pages);
+    fieldset.appendChild(br.cloneNode());
 
-const read = document.createElement("input");
-read.type = "text";
-read.id = "read";
-read.name = "read";
-const rLabel = document.createElement("label");
-rLabel.htmlFor = "read";
-rLabel.textContent = "Read: ";
-fieldset.appendChild(rLabel);
-fieldset.appendChild(read);
-fieldset.appendChild(br.cloneNode());
+    const read = document.createElement("input");
+    read.type = "text";
+    read.id = "read";
+    read.name = "read";
+    const rLabel = document.createElement("label");
+    rLabel.htmlFor = "read";
+    rLabel.textContent = "Read: ";
+    fieldset.appendChild(rLabel);
+    fieldset.appendChild(read);
+    fieldset.appendChild(br.cloneNode());
 
-const post = document.createElement("button");
-post.type = "sumbit";
-post.textContent = "Add book";
-fieldset.appendChild(post);
+    const post = document.createElement("button");
+    post.type = "sumbit";
+    post.textContent = "Add book";
+    fieldset.appendChild(post);
 
-post.addEventListener("click", (event) => {
-    document.querySelectorAll("input").forEach(input => myLibrary.push(input.value));
-    event.preventDefault();
 
-});
+    post.addEventListener("click", (event) => {
+        event.preventDefault();
+        myLibrary.push(title.value, author.value, pages.value, read.value);
+        card1.textContent = `${title.value}, ${author.value}, ${pages.value}, ${read.value}`;
+    });   
+}
 
 
